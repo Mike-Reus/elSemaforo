73e7fb8197880182bc5724095ed72507c003a2cf
Mike <5188083@gmail.com>
2021-08-18 17:43:39 +0200

    second death)

--- elSemaforo/ViewController.swift
+++ elSemaforo/ViewController.swift
@@ -11,10 +11,18 @@ class ViewController: UIViewController {
 
     @IBOutlet var redRoundShow: UIView!
     
+    @IBOutlet var yellowRoudShow: UIView!
+    
+    @IBOutlet var greenRoundShow: UIView!
+    
     @IBOutlet var showBottom: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redRoundShow.layer.cornerRadius = 60
+        yellowRoudShow.layer.cornerRadius = 60
+        greenRoundShow.layer.cornerRadius = 60
+        
     }
 
     @IBAction func pressButtom() {
